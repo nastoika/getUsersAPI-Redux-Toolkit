8b6c8731f0f6fa37f26519e9ac37b508f98432d3
nastoika <nastya.gorlachyova@mail.ru@>
2023-09-04 18:31:06 +0300

    add custom hooks

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,10 @@
 import React from 'react';
 import './App.css';
+import {useAppSelector} from "./hooks/redux";
 
 function App() {
+
+  // const {} = useAppSelector(state => state.)
   return (
     <div className="App">
 
